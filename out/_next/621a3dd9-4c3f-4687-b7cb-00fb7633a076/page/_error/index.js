19e98a0f09401b70f22ad9a1053da1c5cb5dd427
Adrian Li <li.adrianmc@gmail.com>
2017-11-04 02:56:07 -0700

    new build

--- out/_next/621a3dd9-4c3f-4687-b7cb-00fb7633a076/page/_error/index.js
+++ out/_next/621a3dd9-4c3f-4687-b7cb-00fb7633a076/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[217]);
+            var comp = module.exports=webpackJsonp([4],[],[228]);
             return { page: comp.default }
           })
         
--- out/_next/621a3dd9-4c3f-4687-b7cb-00fb7633a076/page/index.js
+++ out/_next/621a3dd9-4c3f-4687-b7cb-00fb7633a076/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[216]);
+            var comp = module.exports=webpackJsonp([3],[],[227]);
             return { page: comp.default }
           })
         
