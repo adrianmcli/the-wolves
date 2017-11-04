37551552bf363c9cff3c627fe78c3137ac53784f
Adrian Li <li.adrianmc@gmail.com>
2017-11-04 00:56:49 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.div`
+  color: red;
+`;
+
+export default () => <div><Title>Hello</Title> from Next.js</div>;
